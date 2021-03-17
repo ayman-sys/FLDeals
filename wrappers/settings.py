96ae122be6bc33a6899b7d0f3a9ec439acb83c30
Ayman Shaikh <67065247+ayman-sys@users.noreply.github.com>
2021-03-17 23:50:41 +0530

    Update settings.py

--- wrappers/settings.py
+++ wrappers/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$a^w$eh7g*&@dt%hfv7qu*a87u(b3$(5ng(f@bj@%9a*2_z+te'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','fldeals.herokuapp.com']
 
